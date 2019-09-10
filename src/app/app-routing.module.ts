a057e2e6c22ca16f67bd3931da7327369818017e
P1NHE4D <aleks.michael.g2@googlemail.com>
2019-09-10 15:25:46 +0200

    Project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainMenuComponent} from "./components/main-menu/main-menu.component";
+import {SingleplayerComponent} from "./components/singleplayer/singleplayer.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainMenuComponent},
+  { path: 'singleplayer', component: SingleplayerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { SingleplayerComponent } from './components/singleplayer/singleplayer.component';
+import { GameComponent } from './components/game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    SingleplayerComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
