2ec2561c958e4519b2dd92e0ea80490bc0854e22
P1NHE4D <aleks.michael.g2@googlemail.com>
2019-09-11 20:25:15 +0200

    Implemented enemy movement Implemented bullet intersection method

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -18,6 +18,9 @@ export class GameComponent implements OnInit {
   private enemyBulletImg: HTMLImageElement;
   private androidAlienImg: HTMLImageElement;
   private images: HTMLImageElement[];
+  private enemyMovementDirection: Direction;
+  private enemyReachedBoundary: boolean;
+  private enemyMovementCooldown: number;
 
 
   constructor() {
@@ -28,6 +31,9 @@ export class GameComponent implements OnInit {
     this.ctx = this.canvas.getContext('2d');
     this.ctx.canvas.width = 700;
     this.ctx.canvas.height = 400;
+    this.enemyMovementDirection = Direction.RIGHT;
+    this.enemyMovementCooldown = 15;
+    this.enemyReachedBoundary = false;
     this.loadImages();
   }
 
@@ -68,6 +74,20 @@ export class GameComponent implements OnInit {
       this.playerOneImg
     );
     this.ctx.drawImage(this.playerOneImg, this.playerOne.getX(), this.playerOne.getY());
+    this.enemies = [];
+    let rowSize: number = (this.ctx.canvas.width - (this.ctx.canvas.width % this.androidAlienImg.width) - 2 * this.androidAlienImg.width);
+    let enemiesPerRow: number =  rowSize / (this.androidAlienImg.width + 10);
+
+    // Spawn enemies
+    for (let j = 0; j < 4; ++j) {
+      let y: number = (this.androidAlienImg.width + 10) * j;
+      for (let i = 0; i < enemiesPerRow; ++i) {
+        let x: number = this.androidAlienImg.width + (this.androidAlienImg.width + 10) * i;
+        let enemy = new Enemy(x, y, this.androidAlienImg.width, this.androidAlienImg.height, 0, this.ctx.canvas.width - this.androidAlienImg.width, this.ctx.canvas.height - this.androidAlienImg.height);
+        this.enemies.push(enemy);
+      }
+    }
+
   }
 
   // TODO: Add keys for playerTwo
@@ -95,18 +115,64 @@ export class GameComponent implements OnInit {
   // TODO: Add enemies
   gameLoop = () => {
     requestAnimationFrame(this.gameLoop);
+
+    // Player movement
     this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     this.ctx.drawImage(this.playerOneImg, this.playerOne.getX(), this.playerOne.getY());
+
+    // Bullet movement
     for(let bullet of this.bullets) {
       bullet.move(Direction.UP, () => {
-        const index = this.bullets.indexOf(bullet);
+        let index = this.bullets.indexOf(bullet);
         this.bullets.splice(index, 1);
       });
-      this.ctx.fillStyle = "#FFFFFF";
-      this.ctx.font = "24pt Impact";
-      this.ctx.fillText(`Bullets: ${this.bullets.length}`, 100, 100);
       this.ctx.drawImage(this.bulletImg, bullet.getX(), bullet.getY());
     }
+
+
+    //Check for bullet intersection
+    for(let bullet of this.bullets) {
+      for(let enemy of this.enemies) {
+        if(bullet.intersectsWithObject(enemy.getX(), enemy.getWidth(), enemy.getY(), enemy.getHeight())) {
+          let index = this.enemies.indexOf(enemy);
+          this.enemies.splice(index, 1);
+          index = this.bullets.indexOf(bullet);
+          this.bullets.splice(index, 1);
+        }
+      }
+    }
+
+    // Enemy movement
+    if ((this.enemyMovementCooldown -= 1) === 0) {
+      this.enemyMovementCooldown = 15;
+
+      for(let enemy of this.enemies) {
+        if (enemy.boundaryReached(this.enemyMovementDirection)) {
+          this.enemyReachedBoundary = true;
+        }
+      }
+
+      if (this.enemyReachedBoundary) {
+        for(let enemy of this.enemies) {
+          enemy.move(Direction.DOWN);
+        }
+        if (this.enemyMovementDirection === Direction.RIGHT) {
+          this.enemyMovementDirection = Direction.LEFT;
+        } else {
+          this.enemyMovementDirection = Direction.RIGHT;
+        }
+        this.enemyReachedBoundary = false;
+      } else {
+        for(let enemy of this.enemies) {
+          enemy.move(this.enemyMovementDirection);
+        }
+      }
+    }
+
+    for (let enemy of this.enemies) {
+      this.ctx.drawImage(this.androidAlienImg, enemy.getX(), enemy.getY());
+    }
+
   };
 
 }
@@ -212,6 +278,10 @@ export class Bullet {
     }
   }
 
+  public intersectsWithObject(xPos: number, width: number, yPos: number, height: number) {
+    return this.xPos >= xPos && this.xPos <= xPos + width && this.yPos <= yPos && this.yPos >= yPos - height;
+  }
+
   /**
    * Returns the x-position of the bullet
    */
@@ -227,34 +297,89 @@ export class Bullet {
   }
 }
 
+export class Enemy {
+  private readonly movementSpeed: number = 6;
+  private cooldown: number = 10;
+
+  /**
+   * Instantiates a new enemy
+   * @param xPos initial x-position of enemy
+   * @param yPos initial y-position of enemy
+   * @param leftBoundary
+   * @param rightBoundary
+   * @param lowerBoundary
+   */
+  constructor(
+    private xPos: number,
+    private yPos: number,
+    private width: number,
+    private height: number,
+    private leftBoundary: number,
+    private rightBoundary: number,
+    private lowerBoundary: number
+  ) {}
+
+  /**
+   * Moves the enemy in the desired direction by a predefined value
+   * @param direction
+   */
+  public move(direction: Direction): void {
+    switch(direction) {
+      case Direction.RIGHT:
+        if (this.xPos + this.movementSpeed < this.rightBoundary) {
+          this.xPos += this.movementSpeed;
+        }
+        break;
+      case Direction.LEFT:
+        if (this.xPos - this.movementSpeed > this.leftBoundary) {
+          this.xPos -= this.movementSpeed;
+        }
+        break;
+      case Direction.DOWN:
+        if (this.yPos + this.movementSpeed < this.lowerBoundary) {
+          this.yPos += this.movementSpeed;
+        }
+        break;
+    }
+  }
+
+  public boundaryReached(direction: Direction): boolean {
+    switch(direction) {
+      case Direction.RIGHT:
+        return this.xPos + this.movementSpeed >= this.rightBoundary;
+      case Direction.LEFT:
+        return this.xPos - this.movementSpeed <= this.leftBoundary;
+      case Direction.DOWN:
+        return this.yPos + this.movementSpeed >= this.lowerBoundary;
+    }
+  }
+
+  /**
+   * Returns x-position of enemy
+   */
+  public getX(): number {
+    return this.xPos;
+  }
+
+  /**
+   * Returns y-position of enemy
+   */
+  public getY(): number {
+    return this.yPos;
+  }
+
+  public getWidth(): number {
+    return this.width;
+  }
+
+  public getHeight(): number {
+    return this.height;
+  }
+}
+
 export enum Direction{
   DOWN,
   UP,
   LEFT,
   RIGHT
-}
-
-export class Enemy {
-
-  constructor(
-    private xPos: number,
-    private yPos: number
-  ) {}
-
-  public move(): void {
-
-  }
-
-  public getX() {
-    return this.xPos;
-  }
-
-  public getY(): number {
-    return this.yPos;
-  }
-
-  // TODO: Implement method
-  public getCenteredX(): number {
-    return 0;
-  }
 }
