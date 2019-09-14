b7b1888fc8cb16ddc900d28d9bc7ddc3be6d2d11
P1NHE4D <aleks.michael.g2@googlemail.com>
2019-09-14 18:19:07 +0200

    Added loader-service

---
+++ src/app/services/loader.service.ts
@@ -0,0 +1,105 @@
+import { Injectable } from '@angular/core';
+import { Subject } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoaderService {
+  private loadCount: number;
+  private resourceCount: number;
+  private loadedImages: Map<string, HTMLImageElement>;
+  private loadedAudioFiles: Map<string, HTMLAudioElement>;
+  private resourcesLoadedSource = new Subject<number>();
+
+  constructor() {
+    this.loadCount = 0;
+    this.resourceCount = 0;
+    this.loadedImages = new Map<string, HTMLImageElement>();
+    this.loadedAudioFiles = new Map<string, HTMLAudioElement>();
+  }
+
+  resourcesLoaded$ = this.resourcesLoadedSource.asObservable();
+
+  /**
+   * @return a map containing all loaded images
+   */
+  getImages(): Map<string, HTMLImageElement> {
+    return this.loadedImages;
+  }
+
+  /**
+   * @return a map containing all loaded audio files
+   */
+  getAudioFiles(): Map<string, HTMLAudioElement> {
+    return this.loadedAudioFiles;
+  }
+
+  /**
+   * Pre-loads static files used in the application
+   * Supported file types: image, audio
+   * @param resources FileDict array containing the resources to be loaded
+   */
+  preload(resources: FileDict[]): void {
+    let onResourceLoaded = () => ++this.loadCount;
+
+    for (let resource of resources) {
+      ++this.resourceCount;
+      this.load(resource, onResourceLoaded, () => {
+        throw new Error(`Failed to load resource ${resource.src}`)
+      });
+    }
+    this.resourcesLoaded();
+  }
+
+  private resourcesLoaded(): void {
+    if (this.loadCount === this.resourceCount) {
+      this.resourcesLoadedSource.next();
+    } else {
+      setTimeout(() => {
+        this.resourcesLoaded();
+      }, 10);
+    }
+  }
+
+  private preloadImage(img: FileDict, onload: () => any, onerror: () => any): void {
+    this.loadedImages.set(img.name, new Image());
+    this.loadedImages.get(img.name).onload = () => {
+      onload();
+    };
+    this.loadedImages.get(img.name).onerror = () => {
+      onerror();
+    };
+    this.loadedImages.get(img.name).src = img.src;
+  }
+
+  private preloadAudio(audio: FileDict, onload: () => any, onerror: () => any): void {
+    this.loadedAudioFiles.set(audio.name, new Audio());
+    this.loadedAudioFiles.get(audio.name).onload = () => {
+      onload();
+    };
+    this.loadedAudioFiles.get(audio.name).onerror = () => {
+      onerror();
+    };
+    this.loadedAudioFiles.get(audio.name).src = audio.src;
+  }
+
+  private load(resource, onload: () => any, onerror: () => any): void {
+    switch(resource.type) {
+      case "image":
+        this.preloadImage(resource, onload, onerror);
+        break;
+      case "audio":
+        this.preloadAudio(resource, onload, onerror);
+        break;
+      default:
+        throw new Error(`Type ${resource.type} is not supported`)
+    }
+  }
+
+}
+
+export interface FileDict {
+  name: string;
+  type: string;
+  src: string;
+}
